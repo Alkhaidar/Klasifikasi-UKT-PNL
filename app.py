8c2c005e42825ec7ded06a1c53f44f2ed23d5b43
unknown <imamsyakrawi@gmail.com>
2024-03-10 22:37:23 +0700

    Upload Terbaru 2

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+app.secret_key= 'your-secret-key'
+
+
+#Meghubungkan Mysql
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'klasifikasi'
+
+mysql = MySQL(app)
+
 
 @app.route ('/')
 def Index ():
-    return render_template('dashboard.html')
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    else:
+        return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login ():
+    if request.method == 'POST':
+        username = request.form['username']
+        pwd = request. form['password']
+        cur = mysql.connection.cursor()
+        cur.execute(f"select username, password from tbl_users where username = '{username}'")
+        user = cur.fetchone()
+        cur.close()
+        if user and pwd == user [1]:
+            session ['username'] = user[0]
+            return redirect(url_for('Index'))
+        else:
+            return render_template('login.html',error='Invalid username or password')
+    return render_template('login.html')
+
 
 @app.route ('/user')
-def login ():
+def user ():
     return render_template('user.html')
 
 @app.route ('/datatraining')
@@ -25,9 +58,6 @@ def dataklasifikasi ():
 def klasifikasi ():
     return render_template('klasifikasi.html')
 
-@app.route ('/login')
-def logout ():
-    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
