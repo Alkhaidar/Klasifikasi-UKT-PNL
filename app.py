83bd3a20ef783819d3e854e35120eca9777f3481
unknown <imamsyakrawi@gmail.com>
2024-03-20 14:34:08 +0700

    Pembuatan Crud Users Fix

--- app.py
+++ app.py
@@ -52,51 +52,77 @@ def insert():
         mysql.connection.commit()
         return redirect(url_for('user'))
 
-@app.route('/delete/<string:id_data>', methods = ['POST','PUT','DELETE'])
+@app.route('/delete/<string:id_data>', methods = ['POST','DELETE'])
 def delateuser(id_data):
-    if(request.form['_method'] == 'PUT'):
-        username = request.form['username']
+    if (request.form['_method'] == 'DELETE'):
+        flash("Delete Data Berhasil")
         cur = mysql.connection.cursor()
-        cur.execute("DELETE FROM tbl_users WHERE id = %s", (id_data))
-        mysql.connection.commit()
-        cur.close()
-        
-        return redirect (url_for('user'))
-    elif(request.form['_method'] == 'DELETE'):
-        cur = mysql.connection.cursor()
-        cur.execute("DELETE FROM tbl_users WHERE id = %s", (id_data))
+        cur.execute("DELETE FROM tbl_users WHERE id = %s", (id_data,))
         mysql.connection.commit()
         cur.close()
         return redirect (url_for('user'))
+
+@app.route('/user/reset/<user_id>', methods=['POST', 'PUT'])
+def putUserReset(user_id):
+    password = request.form['password']
+    password2 = request.form['password2']
     
+    if(password == password2):
+        flash(" Change Password Berhasil")
+        cur = mysql.connection.cursor()
+        cur.execute("""UPDATE tbl_users set password=%s WHERE id=%s""", (password, user_id))
+        mysql.connection.commit()
+        cur.close()
+    return redirect(url_for('user'))
+
+
 @app.route ('/user')
 def user ():
-
-    cur = mysql.connection.cursor()
-    cur.execute ("SELECT * From tbl_users")
-    data =cur.fetchall()
-    cur.close()
-    return render_template('user.html', tbl_users = data, nama=session['nama'] )
-    
+    if 'nama' in session:
+        cur = mysql.connection.cursor()
+        cur.execute ("SELECT * From tbl_users")
+        data =cur.fetchall()
+        cur.close()
+        return render_template('user.html', tbl_users = data, nama=session['nama'] )
+    else:
+        return render_template('login.html')
+   
 
 
 
 @app.route ('/datatraining')
 def datatraining ():
-    return render_template('datatraining.html', nama=session['nama'])
-
+    if 'nama' in session:
+        return render_template('datatraining.html', nama=session['nama'])
+    else:
+        return render_template('login.html')
+   
 @app.route ('/datatesting')
 def datatesting():
-    return render_template('datatesting.html', nama=session['nama'])
+    if 'nama' in session:
+        return render_template('datatesting.html', nama=session['nama'])
+    else:
+        return render_template('login.html')
 
 @app.route ('/dataklasifikasi')
 def dataklasifikasi ():
-    return render_template('dataklasifikasi.html', nama=session['nama'])
+    if 'nama' in session:
+        return render_template('dataklasifikasi.html', nama=session['nama'])
+    else:
+        return render_template('login.html')
 
 @app.route ('/klasifikasi')
 def klasifikasi ():
-    return render_template('klasifikasi.html')
-
+    if 'nama' in session:
+     return render_template('klasifikasi.html')
+    else:
+        return render_template('login.html')
+    
+@app.route('/logout')
+def logout():
+    if 'nama' in session:
+        session.pop('nama', None)
+    return redirect('/login')
 
 if __name__ == "__main__":
     app.run(debug=True)
